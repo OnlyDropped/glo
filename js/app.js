3812c88c0704dd4e076affd8cd73e89909201fbb
OnlyDropped <asheburenkov@yandex.ru>
2022-01-29 20:20:16 +0300

    added main, slider

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$('.reviews-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  //customPaging:'35px',
+  variableWidth: true,
+  centerMode: true,
+  //centerPadding: '60px',
+  //easing: 'easeInOutBack',
+  arrows: true,
+  //cssEase: 'cubic-bezier(0.950, 0.050, 0.795, 0.035)',
+  //cssEase: '4.9s cubic-bezier(.96,.04,.43,.2)'
+});
