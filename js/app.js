3ab81a8264d16a46ea112ac69c891dbb06d54970
OnlyDropped <asheburenkov@yandex.ru>
2022-01-30 01:03:13 +0300

    added media 768px

--- js/app.js
+++ js/app.js
@@ -9,4 +9,24 @@ $('.reviews-slider').slick({
   arrows: true,
   //cssEase: 'cubic-bezier(0.950, 0.050, 0.795, 0.035)',
   //cssEase: '4.9s cubic-bezier(.96,.04,.43,.2)'
+    responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '10px',
+        slidesToShow: 1
+      }
+    }
+  ]
 });
